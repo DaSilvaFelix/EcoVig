1917896e330c28e6eb3499d322e8fa9526148467
Felix Polo <brunolok@gmail.com>
2024-08-08 14:27:04 -0300

    cuarto commint

--- backend/src/routers/general.routes.js
+++ backend/src/routers/general.routes.js
@@ -3,7 +3,6 @@ const mostrarCountries = require("../controllers/countries.controllers");
 const mostrarStates = require("../controllers/states.controllers");
 const register = require("../controllers/register");
 const login = require("../controllers/login.controllers");
-const climasLocal = require("../controllers/climas");
 
 router.get("/", (req, res) => {
   res.send("servidor en la red");
@@ -12,6 +11,6 @@ router.get("/countries", mostrarCountries);
 router.get("/states/:id", mostrarStates);
 router.post("/register", register);
 router.post("/login", login);
-router.post("/climas", climasLocal);
+
 
 module.exports = router;
